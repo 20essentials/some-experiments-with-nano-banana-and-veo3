d140c0c4357797a1835adad71b102f57dedc77cc
DominicDev <s0l0estudi4r@gmail.com>
2026-01-15 14:18:09 +0100

    v0.0.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import { Background } from '@/components/background';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
-      <h1 className='bg-amber-300'>Hola</h1>
+      <Background />
+      <section className='relative w-full h-screen flex flex-wrap place-content-center'>
+        <img
+          src='/assets/title.webp'
+          alt='Am Title'
+          className='w-[66%] object-contain mix-blend-overlay'
+        />
+
+        <Link href={'https://gemini.google/it/overview/image-generation/'}>
+          <img
+            src='/assets/nanobana.png'
+            alt='Nanobana Logo'
+            className='w-[4.5vmax] object-contain absolute bottom-[1.5vmax] right-[1.5vmax]'
+          />
+        </Link>
+
+        <Link href={'https://gemini.google/it/overview/video-generation/'}>
+          <img
+            src='/assets/veo-3.png'
+            alt='Veo3 Logo'
+            className='w-[4.5vmax] object-contain absolute top-[1.5vmax] left-[1.5vmax]'
+          />
+        </Link>
+      </section>
     </>
   );
 }
